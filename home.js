2efbb3f85b9f38071c14be31438ea1bf0ae97880
Raman <alexesramon0909@gmail.com>
2021-09-18 16:51:05 +0530

    port removed

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const { ENGINE_METHOD_ALL } = require("constants");
 const express =require("express");
 const app =express();
-const port = process.env.Port||8080;
+const port = process.env.Port;
 
 app.set('view engine','pug');
 
